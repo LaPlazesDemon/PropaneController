e00f75bfb15071189ed23c6cac34dbf04b866d03
LaPlazesDemon <164425143+LaPlazesDemon@users.noreply.github.com>
2024-04-09 11:12:55 -0400

    changed PUT to GET

--- main.py
+++ main.py
@@ -7,27 +7,14 @@ app = FastAPI()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(config.pins["LED"]["num"], GPIO.OUT)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/pins/led/on")
-def read_item():
-    GPIO.output(config.pins["LED"]["num"], GPIO.HIGH)
-    return {True}
-
-@app.put("/gpio/{pin_num}/on")
+@app.get("/gpio/{pin_num}/on")
 def turn_on_gpio(pin_num: int):
     # Turn on GPIO pin
     GPIO.output(pin_num, GPIO.HIGH)
     return {"message": "GPIO turned on"}
 
-@app.put("/gpio/{pin_num}/off")
+@app.get("/gpio/{pin_num}/off")
 def turn_on_gpio(pin_num: int):
     # Turn on GPIO pin
     GPIO.output(pin_num, GPIO.LOW)
     return {"message": "GPIO turned off"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id", item_id, "q", q}
