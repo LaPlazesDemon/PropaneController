d5c3337259fcab0d40bf0382e4ce8b476f0e81cd
LaPlazesDemon <164425143+LaPlazesDemon@users.noreply.github.com>
2024-04-09 21:03:36 -0400

    Added Socket.IO and Python process spawning

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
 const express = require('express');
-const socket = require('socket.io')
+const { createServer } = require("node:http")
+const { Server } = require('socket.io');
+const { spawn } = require('child_process');
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server)
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.hostname} ${req.url}`);
-    next();
-});
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/www/index.html')
-});
-
+// Setup static page distribution (CSS, JS)
 app.use('/static', express.static('www/static'));
 
-app.listen(80, console.log('Webserver listening on port 80'));
+// Log access requests
+app.use((req, res, next) => {console.log(`${req.method} ${req.hostname} ${req.url}`);next();});
+
+// Landing Page
+app.get('/', (req, res) => {res.sendFile(__dirname + '/www/index.html')});
+
+// REST APIs to handle communication from the python server
+// Fuel Valve Status Reciever [1 is open, 0 is closed]
+app.get('/rest/fuelValveStatus', (req, res) => {
+    const state = req.query.state;
+    io.emit('fuelValveStatus', state)
+    res.status(200).send();
+});
+
+// Start Web Server
+server.listen(80, console.log('Webserver listening on port 80'));
+
+//////////////////////
+// PYTHON PROCESSES //
+//////////////////////
+
+// Spawn the Python child process
+const PyWatchdog = spawn('python', ["GPIOWatchdog.py"], {
+    detached: true,
+    stdio: 'ignore'
+});
+
+// Detach the child process
+PyWatchdog.unref();
+console.log('GPIO Watchdog Started.');
+
+// Listen for process exit
+process.on('exit', () => {
+    console.log('Exiting Node.js process. Terminating Python child process.');
+    PyWatchdog.kill();
+});
